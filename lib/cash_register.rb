2eaf532d796eee1d7d5f6a653fae7ba2428c69bc
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-30 22:59:20 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+require 'pry'
+class CashRegister
 
+  attr_accessor :items, :discount, :total, :last_transaction
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(name, price, quantity=1)
+    self.total += price * quantity
+  end
+
+  def apply_discount
+    discount_percent = ((100 - self.discount)/(100))
+    self.total *= discount_percent
+    
+  end
+end

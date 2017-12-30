51c9638c55cf93b97053a7b455540cd333b903a1
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-30 23:08:35 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
     quantity.times do
       items << name
     end
-    self.last_transaction = amount * quantity
+    self.last_transaction = price * quantity
   end
 
   def apply_discount
